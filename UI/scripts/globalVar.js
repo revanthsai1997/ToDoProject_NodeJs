fa0be39b3ff6dd8aa494a7e242667f36779007fd
revanthsai1997 <revanthsaivarma@gmail.com>
2024-04-19 11:01:35 -0500

    changed cors url

--- UI/scripts/globalVar.js
+++ UI/scripts/globalVar.js
@@ -1,2 +1,3 @@
-const domainURL = "https://my-todo-project.cyclic.app/";
+// const domainURL = "https://my-todo-project.cyclic.app/";
 // const domainURL = "http://localhost:3000/";
+const domainURL = "https://todoproject-nodejs.onrender.com/";
